7d82b5e42acb25d70da45f1707a56479859ee95f
e-redun <e.redun@mail.ru>
2022-07-27 00:19:19 +0500

    Добавил комменты в BookService

--- CommonLib/Services/BookService.cs
+++ CommonLib/Services/BookService.cs
@@ -7,31 +7,31 @@ namespace CommonLib.Services
 {
     public class BookService
     {
-        public Book CreateBook(string title,
-                               List<Person> authors,
-                               int volume,
-                               DateTime signedForPrint,
-                               int pages,
-                               FontFamilies fontFamily,
-                               Formats format,
-                               Organization publisher,
-                               Organization tipography,
-                               decimal rubPrice
+        public Book CreateBook(string title,            // Название
+                               List<Person> authors,    // Авторы
+                               int volume,              // Том
+                               DateTime signedForPrint, // Дата отдачи в печать
+                               int pages,               // Всего страниц
+                               FontFamilies fontFamily, // гарнитура
+                               Formats format,          // Формат
+                               Organization publisher,  // Издательство
+                               Organization tipography, // Типография
+                               decimal rubPrice         // цена в рублях
                                )
         {
 
             return new Book()
             {
-                Title = title,
-                Authors = authors,
-                Volume = volume,
-                SignedForPrint = signedForPrint,
-                Pages = pages,
-                FontFamily = fontFamily,
-                Format = format,
-                Publisher = publisher,
-                Tipography = tipography,
-                RubPrice = rubPrice
+                Title = title,                   // Название
+                Authors = authors,               // Авторы
+                Volume = volume,                 // Том
+                SignedForPrint = signedForPrint, // Дата отдачи в печать
+                Pages = pages,                   // Всего страниц
+                FontFamily = fontFamily,         // гарнитура
+                Format = format,                 // Формат
+                Publisher = publisher,           // Издательство
+                Tipography = tipography,         // Типография
+                RubPrice = rubPrice              // цена в рублях
             };
         }
     }
